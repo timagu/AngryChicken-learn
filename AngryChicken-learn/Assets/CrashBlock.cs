e5adbb5dc3c077dcac67d4c8a9078d0e1256f1f9
KoutaYamazawa <kouta0810277@gmail.com>
2018-03-21 16:38:50 +0900

    AngryChicken-learn誤動作
    PlayerとSlingShotがドラッグしてもくっついて動かない。

---
+++ AngryChicken-learn/Assets/CrashBlock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CrashBlock : MonoBehaviour
+{
+    public float m_Strength = 1, m_hp = 1;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        float damage = col.relativeVelocity.magnitude - m_Strength;
+        if (damage > 0)
+        {
+            m_hp -= damage;
+        }
+
+        if (m_hp < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ AngryChicken-learn/Assets/Slingshot/Script/LookatSprite.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AngryChicken2D
+{
+
+	public class LookatSprite : MonoBehaviour
+	{
+		public Transform target;
+
+		void Update()
+		{
+			Vector2 diff = transform.position - target.position;
+			transform.rotation = Quaternion.FromToRotation( Vector3.up, diff.normalized );
+		}
+	}
+
+}
